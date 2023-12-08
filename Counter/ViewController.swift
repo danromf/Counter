42b2248dbdde16790210c687b66eb63ca3484b82
danromf <dan.romanoff15@gmail.com>
2023-12-08 22:02:50 +0700

    fix code-style + add autoscroll uitextview

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,70 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var historyTextView: UITextView!
-    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak private var historyTextView: UITextView!
+    @IBOutlet weak private var counterLabel: UILabel!
     
-    var counter = 0 {
+    private var counter = 0 {
         didSet {
             counterLabel.text = String(counter)
         }
     }
     
-    var historyMessage = "История изменений:\n" {
+    private var historyMessage = "История изменений:\n" {
         didSet {
             historyTextView.text = historyMessage
+            let range = NSRange(location: historyTextView.text.count - 1, length: 0)
+            historyTextView.scrollRangeToVisible(range)
         }
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         historyTextView.text = historyMessage
     }
-
-    @IBAction func increaseCounter(_ sender: UIButton) {
-        counter += 1
-        
-        historyMessage += "\(getCurrentDateTime()): значение изменено на +1\n"
-    }
     
-    @IBAction func decreaseCounter(_ sender: UIButton) {
-        if counter > 0 {
-            counter -= 1
-            historyMessage += "\(getCurrentDateTime()): значение изменено на -1\n"
-        } 
-        else {
-            historyMessage += "\(getCurrentDateTime()): попытка уменьшить значение счетчика ниже 0\n"
-        }
-    }
-    
-    @IBAction func resetCounter(_ sender: UIButton) {
-        counter = 0
-        historyMessage += "\(getCurrentDateTime()): значение сброшено\n"
-    }
-    
-
-    @IBAction func increaseButtonTouchDown(_ sender: UIButton) {
-        sender.tintColor = UIColor(red: 0.06, green: 0.38, blue: 0.14, alpha: 1.00)
-    }
-    
-    @IBAction func increaseButtonTouchUp(_ sender: UIButton) {
-        sender.tintColor = .systemGreen
-    }
-    
-    @IBAction func decreaseButtonTouchDown(_ sender: UIButton) {
-        sender.tintColor = .darkGray
-    }
-    
-    @IBAction func decreaseButtonTouchUp(_ sender: UIButton) {
-        sender.tintColor = .lightGray
-    }
-    
-    func getCurrentDateTime() -> String {
+    private func getCurrentDateTime() -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
         
         return formatter.string(from: Date())
     }
+    
+    @IBAction private func increaseCounter(_ sender: UIButton) {
+        counter += 1
+        
+        historyMessage += "\(getCurrentDateTime()): значение изменено на +1\n"
+    }
+    
+    @IBAction private func decreaseCounter(_ sender: UIButton) {
+        if counter > 0 {
+            counter -= 1
+            historyMessage += "\(getCurrentDateTime()): значение изменено на -1\n"
+        }
+        else {
+            historyMessage += "\(getCurrentDateTime()): попытка уменьшить значение счетчика ниже 0\n"
+        }
+    }
+    
+    @IBAction private func resetCounter(_ sender: UIButton) {
+        counter = 0
+        historyMessage += "\(getCurrentDateTime()): значение сброшено\n"
+    }
+    
+    @IBAction private func increaseButtonTouchDown(_ sender: UIButton) {
+        sender.tintColor = UIColor(red: 0.06, green: 0.38, blue: 0.14, alpha: 1.00)
+    }
+    
+    @IBAction private func increaseButtonTouchUp(_ sender: UIButton) {
+        sender.tintColor = .systemGreen
+    }
+    
+    @IBAction private func decreaseButtonTouchDown(_ sender: UIButton) {
+        sender.tintColor = .darkGray
+    }
+    
+    @IBAction private func decreaseButtonTouchUp(_ sender: UIButton) {
+        sender.tintColor = .lightGray
+    }
+    
 }
 
